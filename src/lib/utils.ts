d698a0e09b8d3fadade43359c6a059fa551756d6
Thiago Henrique Figueredo de Oliveira <thiago.figueredo@sga.pucminas.br>
2024-10-04 10:07:01 -0300

    Repair Error Sonner

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import clsx from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+// Definindo o tipo ClassValue
+type ClassValue = string | undefined | null | boolean | ClassValue[]
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(...inputs))
 }
