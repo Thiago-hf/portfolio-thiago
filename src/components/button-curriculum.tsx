953b304ab7959517e54a331b593058a37ae091dd
Thiago Henrique Figueredo de Oliveira <164034177+Thiago-hf@users.noreply.github.com>
2025-01-07 18:07:57 -0300

    Update button-curriculum.tsx

--- src/components/button-curriculum.tsx
+++ src/components/button-curriculum.tsx
@@ -7,7 +7,7 @@ export function ButtonCurriculum() {
     <div className="flex items-center justify-center">
       <div className="group relative">
         <Link
-          href="/curriculum_thiago_henrique.pdf"
+          href="/public/curriculum.pdf"
           target="_blank"
           download
         >
